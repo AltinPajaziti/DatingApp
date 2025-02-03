9bf2aee2d4b322f81c434a33c1ac0b4c0aab7b36
AltinPajaziti <altinpajaziti935@gmail.com>
2025-02-03 21:29:19 +0100

    Add DatingAppClient folder and Files

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("GetAllUsers")]    
+
+        public IActionResult GetAllUsers()
+        {
+            return Ok("This is the list of users");
+        }   
+    }
+}
