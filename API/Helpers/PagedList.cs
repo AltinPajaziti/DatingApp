6d29909ff67d03e6fc7a80e452d955dcadef2928
AltinPajaziti <altinpajaziti935@gmail.com>
2025-02-25 22:18:39 +0100

    Pagination Service Added

---
+++ API/Helpers/PagedList.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Helpers
+{
+    public  class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items , int count , int pagenumber , int pagesize)
+        {
+            CurrentPage  = pagenumber;
+
+            TotalPages = (int)Math.Ceiling(count / (double)pagesize);
+
+            pagesize = pagesize;
+
+            TotalCount = count;
+
+            AddRange(items);
+
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalCount { get; set; }
+
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int poagenumber , int pagesize)
+        {
+            var count  = await source.CountAsync();
+            var items = await source.Skip((poagenumber - 1) * pagesize).Take(pagesize).ToListAsync();
+
+
+            return new PagedList<T>(items, count, poagenumber, pagesize);
+        }
+
+    }
+}
