87d4a1bd821fb8c88269269a5e8ba4fdbb16fd50
Altin Pajaziti <altinpajaziti935@gmail.com>
2025-03-01 13:52:42 +0100

    ...Fixes

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -13,5 +13,25 @@ namespace API.Data
 
         public DbSet<AppUsser> Users { get; set; }
 
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class UserLike
+    {
+        public AppUsser SourceUser { get; set; } = null!;
+        public int SourceUserId { get; set; }
+        public AppUsser TargetUser { get; set; } = null!;
+        public int TargetUserId { get; set; }
+    }
+}
